ab8aec02082c096c9e2829ce13ce12957daba67f
Natalya Solovyova <ns-66@yandex.ru>
2023-01-02 23:06:39 +0700

    feat: add solution for some functions tasks

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -197,8 +197,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let from = startFrom - 1;
+  return () => {
+    from += 1;
+    return from;
+  };
 }
 
 
